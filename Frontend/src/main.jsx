3943e39de19da366bedbb09721f27a1a54fa7046
sohan.eluri <elurisohan8@gmail.com>
2026-02-14 19:57:01 -0500

    Added TanStack Library

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Authprovider } from './context/AuthProvider.jsx'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    
     <Authprovider>
     <App />
     </Authprovider>
+    
   </StrictMode>,
 )
